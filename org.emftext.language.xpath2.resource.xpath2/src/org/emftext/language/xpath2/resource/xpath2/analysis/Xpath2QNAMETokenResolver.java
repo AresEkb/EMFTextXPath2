f9c2883a1acb2b51ea4708758ac64ae8f07b9b9d
Denis Nikiforov <denis.nikif@gmail.com>
2015-07-10 16:57:52 +0500

    Minor changes

--- org.emftext.language.xpath2.resource.xpath2/src/org/emftext/language/xpath2/resource/xpath2/analysis/Xpath2QNAMETokenResolver.java
+++ org.emftext.language.xpath2.resource.xpath2/src/org/emftext/language/xpath2/resource/xpath2/analysis/Xpath2QNAMETokenResolver.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2013 Denis Nikiforov.
+ * Copyright (c) 2013, 2015 Denis Nikiforov.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,43 +10,27 @@
  */
 package org.emftext.language.xpath2.resource.xpath2.analysis;
 
-import javax.xml.namespace.QName;
+import java.util.Map;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
 
 public class Xpath2QNAMETokenResolver implements org.emftext.language.xpath2.resource.xpath2.IXpath2TokenResolver {
-
-    private final org.emftext.language.xpath2.resource.xpath2.analysis.Xpath2DefaultTokenResolver defaultTokenResolver = new org.emftext.language.xpath2.resource.xpath2.analysis.Xpath2DefaultTokenResolver(true);
-
-    @Override
-    public String deResolve(Object value, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container) {
-        if (value instanceof QName) {
-            QName name = (QName)value;
-            String prefix = name.getPrefix();
-            return prefix.isEmpty() ? name.getLocalPart() : prefix + ":" + name.getLocalPart();
-        }
-        // By default token de-resolving is delegated to the DefaultTokenResolver.
-        String result = defaultTokenResolver.deResolve(value, feature, container, null, null, null);
-        return result;
-    }
-
-    @Override
-    public void resolve(String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, org.emftext.language.xpath2.resource.xpath2.IXpath2TokenResolveResult result) {
-        String[] parts = lexem.split(":");
-        switch (parts.length) {
-        case 1:
-            result.setResolvedToken(new QName(parts[0]));
-            break;
-        case 2:
-            result.setResolvedToken(new QName(null, parts[1], parts[0]));
-            break;
-        default:
-            // By default token resolving is delegated to the DefaultTokenResolver.
-            defaultTokenResolver.resolve(lexem, feature, result, null, null, null);
-            break;
-        }
-    }
-
-    @Override
-    public void setOptions(java.util.Map<?,?> options) {
-        defaultTokenResolver.setOptions(options);
-    }
+	
+	private org.emftext.language.xpath2.resource.xpath2.analysis.Xpath2DefaultTokenResolver defaultTokenResolver = new org.emftext.language.xpath2.resource.xpath2.analysis.Xpath2DefaultTokenResolver(true);
+	
+	public String deResolve(Object value, EStructuralFeature feature, EObject container) {
+		// By default token de-resolving is delegated to the DefaultTokenResolver.
+		String result = defaultTokenResolver.deResolve(value, feature, container, null, null, null);
+		return result;
+	}
+	
+	public void resolve(String lexem, EStructuralFeature feature, org.emftext.language.xpath2.resource.xpath2.IXpath2TokenResolveResult result) {
+		// By default token resolving is delegated to the DefaultTokenResolver.
+		defaultTokenResolver.resolve(lexem, feature, result, null, null, null);
+	}
+	
+	public void setOptions(Map<?,?> options) {
+		defaultTokenResolver.setOptions(options);
+	}
+	
 }
